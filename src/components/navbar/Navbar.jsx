8e40909366b2bacc5266df2069a1be1b6d8c8bef
Santiago Chimko <santiagochimko@gmail.com>
2023-10-13 15:07:59 -0300

    Sidebar Animation

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{duration:0.5}}
+        >
+          Santiago Chimko
+        </motion.span>
+        <div className="social">
+          <a href="">
+            <img src="/github.png" alt="github" />
+          </a>
+          <a href="">
+            <img src="/linkedin.png" alt="linkedin" />
+          </a>
+          <a href="">
+            <img src="/dribbble.png" alt="dribbble" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
